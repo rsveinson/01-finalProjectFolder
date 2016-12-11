decb6d8f12a073968b1ae051ce02da3a679a98f8
rsveinson <rsveinson@mts.net>
2016-12-11 11:17:15 -0600

    final project code v2
    roomba/arduino code v2
    follow a line with no controls in place. should demonstrate large,
    consistent oscillation. oscillations won’t diminish over time.

--- projectFinalV2/projectFinalV2.ino
+++ projectFinalV2/projectFinalV2.ino
@@ -2,6 +2,7 @@
  * Rob Sveinson
  * Comp 444 final project
  * Line following roomba
+ * version 2: follow a line without any controls in place
  * 
  * Using a Sparkfun line foolower array and a sonar sensor get a roomba to follow
  * a dark line on a light bachground. This will be developed in 5 stages as follows;
@@ -27,9 +28,8 @@
  * allow the system to settle more quickly by eliminating, or reducing, overshoot, meaning the 
  * robot will start to correct before it passes the set point.
  * 
- * This is stage 0: run the roomba in a straight line and stop when it gets to withing
- * about 20cm of an obstacle. This safety zone may need some adjustment later but that will
- * be a pretty simple matter.
+ * This is stage 2: follow aline without any control in place. Should produce large, consisten 
+ * oscillations, oscillations will not diminish
  */
 
 #include<SoftwareSerial.h>
@@ -38,7 +38,7 @@
 // global scope
 
 const float PIE = 3.14159;        // value of pi used in calculating angles
-const int SAFEDIST = 10;          // 20cm safety zone, used for sonar
+const int SAFEDIST = 20;          // 20cm safety zone, used for sonar
 
 // pin assignments for arduino to roomba 7 mini-din port
 
@@ -81,71 +81,25 @@ void setup(){
   wakeUp(); // 2 second delay built into the wakeUp() function
 
   // ** set roomba mode
-
-  mySerial.write(131);        // put roomba into safe mode
-  delay(20);                  // clean light should go out
-                              // roomba is now ready to receive commands through the din port
+  setMode(128);           // put roomba into passive mode
+  setMode(131);           // put roomba into safe mode
+                          // roomba is now ready to receive commands
                               
-  //readyDance();               // jiggle to signal ready to go
-  playBeep();                 // beep once to indicate waiting for button press
-  waitForButton();            // wait for the button to be pushed
+  //readyDance();         // jiggle to signal ready to go
+  playBeep();             // beep once to indicate waiting for button press
+  waitForButton();        // wait for the button to be pushed
   
   //Serial.println("roomba initialized into safe mode, ready to go");
-/*
-  mySerial.write(byte(137));
-  mySerial.write(byte(0));
-  mySerial.write(byte(100));
-  mySerial.write(byte(0x80));
-  mySerial.write(byte (0));    // go forward
-*/
-  //goForward();
-  //driveWheels(150, 250);
-  //delay(2000);
-  //stopDrive();
-  //Serial.println("after forward and stop");
-
-
-
-  //driveWheels(250,150);
-  //delay(2000);   // for 2 seconds
-  //stopDrive();
-  //delay(500);
-
-  //driveWheels(150, 150);
-  //delay(2000);
-
-  //driveWheels(-250, -250);
-  //delay(2000);
-  //Serial.println("after reverse");
-/*  
-  mySerial.write(byte(137));
-  mySerial.write(byte(0));
-  mySerial.write(byte(0));
-  mySerial.write(byte(0));
-  mySerial.write(byte(0));
-  delay(1000);
-*/
-
-  
-
-  //goForward();
-  //delay(2000);
-
-  //Serial.println("shutting down, (off mode, opcode 173).");
-  //mySerial.write(173);        // change to off mode.
-
-  //float tempAngle = getAngle();      // make sure sensor packet 20 is zeroed out
-                                
 } // end setup
 
 void loop(){
-  float tempAngle = 0.0;
+  
   // send a high and low ping from the sonar sensor
   ping();
-  distance = calcDistance();
+  distance = calcDistance();    // calculate the distance to an object
   
 // drive forward until an obstacle is detected
-  while((int)distance > 10){
+  while((int)distance > SAFEDIST){
    driveWheels(150, 150); 
    ping();
    distance = calcDistance();
@@ -156,7 +110,7 @@ void loop(){
   delay(2000);
   
   // turn CW about 180 degrees
-  turnCCW(183);
+  //turnCCW(183);
 
   ping();
   distance = calcDistance();
@@ -170,11 +124,8 @@ void loop(){
   } // end while
   stopDrive();
   delay(2000);
-  
-  // turn CW about 180 degrees
-  turnCCW(183);
-  
-  //stopDrive();
+ 
+  stopDrive();
 
 /*  
   while((int)distance < 100){
@@ -193,54 +144,6 @@ void loop(){
 /*************************************************
 * receive somar echo and calculate distance
 ***************************************************/
-void turnCCW(int limit){
-  float totalRotation = 0.0;
-  float temp = getAngle();             // make sure angle sensor is zeroed out
-
-  driveWheels(100, -100);  
-  
-  while(totalRotation <= limit){
-    delay(15);
-    Serial.print("total rotation since last update: ");
-    Serial.println(totalRotation);
-    totalRotation += getAngle();
-  }
-} // end turnCW
-
-float getAngle(void){
-
-  int i = 0;
-  float rotation = 0.0;
-  
-  
-  mySerial.write(142);
-  mySerial.write(20);
-  //delay(10);
-
-// 2 bytes, angle high byte, angle low byte
-  while(mySerial.available()){
-    encoderData[i] = mySerial.read();
-    //Serial.println("angle data read");
-    i++;
-  } // end while
-/*
-  for(int j = 0; j < i; j++){
-    Serial.print("data byte # ");
-    Serial.print(j);
-    Serial.print("  ");
-    Serial.println(encoderData[j]);
-  }  // end for
-*/
-  
-  //angleDegrees = (int)(encoderData[0] << 8)|(int)(encoderData[1]&0xFF);
-  rotation = (int)word((int)encoderData[0], (int)encoderData[1]);
-  rotation /= 0.324056; 
-  //Serial.print("degrees turned = ");
-  //Serial.println(angleDegrees);
-
-   i = 0;
-   return rotation;
-} // end get Angle
 
 void getInfo(void){
   if (mySerial.available())   //First order of business: listen to Roomba
@@ -262,7 +165,7 @@ float calcDistance(void){
 } // end ping
 
 /*************************************************
-* send a high and low pint from the sonar sensor
+* send a high and low ping from the sonar sensor
 ***************************************************/
 void ping(void){
   pinMode(SIG, OUTPUT);
@@ -273,27 +176,10 @@ void ping(void){
   
 /************************************************
  * beep to indicate roomba is waiting for a button press
- */
+ ***************************************************/
  void playBeep(void){
-
-   mySerial.write("\x8c\x01\x04\x42\x20\x3e\x20\x42\x20\x3e\x20"); // [140] [1] [4] [68] [32] ... place "start sound" in slot 1
+   mySerial.write("\x8c\x01\x04\x42\x20\x3e\x20\x42\x20\x3e\x20"); // [140] [1] [4] [68] [32]
    mySerial.write("\x8d\x01"); // [141] [1] play it (in slot 1)
- 
- // byte song3[5]={140, 3, 1, 64, 16};
-  //byte playSong3[2]={141, 3};
-  //mySerial.write(song3, 5);
-  //mySerial.write(playSong3, 2);
-/*
-  mySerial.write(140);
-  mySerial.write(3);
-  mySerial.write(1);
-  mySerial.write(64);
-  mySerial.write(16);
-
-  mySerial.write(141);
-  mySerial.write(3);
-*/  
-  
  } // end playBeed
 
 /**********************************************
@@ -322,14 +208,19 @@ void waitForButton(void){
 } // end of waitForButton()
 
 /*******************************************
- * wake up roomba by pulsing dd pin 5 high low high
+ * send code 137 to roomba to stop all motors
  ********************************************/
 void stopDrive(void){
   mySerial.write(byte(137));
   mySerial.write(byte(0));
   mySerial.write(byte(0));
   mySerial.write(byte(0));
-  mySerial.write(byte(0));  
+  mySerial.write(byte(0));
+
+// shut down the oi, roomba won't respond to any commands
+// cycle the power to re-set mode.
+  mySerial.write(byte(173));     
+                                
 } // end stopdrive()
 
 /***********************************************
@@ -342,42 +233,24 @@ void stopDrive(void){
  * 
  * constrain is used to ensure that the parameter values are within 
  * range for driving roomba's wheels
- */
+ ******************************************************/
 void driveWheels(int right, int left)
 {
   // compensate for left pull
-  float comp = left * 0.1;
+  float comp = left * 0.1;              // compensate for a left pull in my roomba
   left += (int)comp;
-  Serial.print("left comp speed ");
-  Serial.println(left);
+  //Serial.print("left comp speed ");
+  //Serial.println(left);
   constrain(right, -500, 500);
   constrain(left, -500, 500);
-
-  Serial.println("in driveWheels");
   
-  mySerial.write(145);
-  mySerial.write(right >> 8);
-  mySerial.write(right);
-  mySerial.write(left >> 8);
-  mySerial.write(left);
+  mySerial.write(145);            // send code for dirct drive
+  mySerial.write(right >> 8);     // send right velocity high byte
+  mySerial.write(right);          // send right velocity low byte
+  mySerial.write(left >> 8);      // send left velocity high byte
+  mySerial.write(left);           // send left velocity low byte
  } // end drive wheels
 
- /* goForward sends the opcode 137 drive 
-  *  the 4 bytes are
-  *  velocity high byte, low byte
-  *  radius high byte, low byte
-  *  this op code doesn't drive the wheel motors directly
-  */
- void goForward(void)
- {
-  //Serial.println("in goForward");
-    mySerial.write(byte(137));
-    mySerial.write(byte(0));
-    mySerial.write(byte(100));
-    mySerial.write(byte(0x80));
-    mySerial.write(byte (0));    // go forward
- } // end goForward
- 
  void wakeUp (void)
 {
   //setWarningLED(ON);
@@ -389,5 +262,10 @@ void driveWheels(int right, int left)
   digitalWrite(ddPin, HIGH);
   delay(2000);
 }
+
+void setMode(int modeCode){
+  mySerial.write(modeCode);       // put roomba into passive mode
+  delay(20);                      // wait for mode change to propegate
+} // end setMode
  
 
